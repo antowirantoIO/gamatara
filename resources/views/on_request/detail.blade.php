39fffa13429a31e291776fb7725c0f6601a0481b
asukayuki7 <asukayuki7@gmail.com>
2023-10-27 12:23:32 +0700

    fixing

--- resources/views/on_request/detail.blade.php
+++ resources/views/on_request/detail.blade.php
@@ -131,7 +131,7 @@
                                                             <button type="button" id="tambahKeluhan" data-id-keluhan="" class="btn btn-primary">Save</button>
                                                         </div>
                                                     @else
-                                                        @if($keluhan != null && $count == $keluhan) 
+                                                        @if($count == $keluhan) 
                                                             <div class="flex-grow-1 d-flex align-items-center justify-content-end">
                                                                 <button type="button" id="tambahKeluhan" class="btn btn-primary" disabled>Save</button>
                                                             </div>

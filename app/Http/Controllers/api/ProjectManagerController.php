2131d505c6dbe1e9ad5b885a024ef051ddddc490
asukayuki7 <asukayuki7@gmail.com>
2023-11-14 11:59:19 +0700

    fixing api

--- app/Http/Controllers/api/ProjectManagerController.php
+++ app/Http/Controllers/api/ProjectManagerController.php
@@ -48,7 +48,7 @@ class ProjectManagerController extends Controller
     public function detailPM(Request $request)
     {
         try{                  
-            $data = OnRequest::with(['complaint','complaint.vendors:id,name','customer:id,name','pm','pa','pe','pe2'])
+            $data = OnRequest::with(['complaint','complaint.vendors:id,name','customer:id,name','pm.karyawan:id,name','pa.karyawan:id,name','pe.karyawan:id,name','pe2.karyawan:id,name','lokasi:id,name'])
                         ->where('id',$request->id)
                         ->first();
          

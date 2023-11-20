248ac6c0a1c229d3acb91640c5483269067c6c8a
asukayuki7 <asukayuki7@gmail.com>
2023-11-20 18:00:42 +0700

    fixingk

--- app/Http/Controllers/api/ProjectManagerController.php
+++ app/Http/Controllers/api/ProjectManagerController.php
@@ -206,7 +206,7 @@ class ProjectManagerController extends Controller
     {
         try{
             $data = ProjectPekerjaan::with('pekerjaan:id,name')->select('id','id_pekerjaan','deskripsi_pekerjaan')
-                    ->where('id_project', $request->id_project)->where('id_subkategori', $request->id_subkategori)
+                    ->where('id',$request->id)->where('id_project', $request->id_project)->where('id_subkategori', $request->id_subkategori)
                     ->orderBy('created_at','desc')
                     ->get();
 

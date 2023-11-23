b084d12a2ffec6d27d20a78e61cfc37c4be950c1
asukayuki7 <asukayuki7@gmail.com>
2023-11-23 19:50:28 +0700

    fixing api

--- app/Http/Controllers/api/BodController.php
+++ app/Http/Controllers/api/BodController.php
@@ -320,14 +320,13 @@ class BodController extends Controller
             $id_project = $request->id_project;
             $id_kategori = $request->id_kategori;
 
-            $progress = ProjectPekerjaan::select('project_pekerjaan.id', 'project_pekerjaan.deskripsi_subkategori', 
-                    'project_pekerjaan.status','project_pekerjaan.id_subkategori','sub_kategori.name')
-                    ->join('sub_kategori', 'project_pekerjaan.id_subkategori', '=', 'sub_kategori.id')
-                    ->where('project_pekerjaan.id_project', $id_project)
-                    ->where('project_pekerjaan.id_kategori', $id_kategori)
-                    ->groupBy('project_pekerjaan.id', 'project_pekerjaan.deskripsi_subkategori', 'sub_kategori.name')
-                    ->filter($request)
-                    ->get();
+            $progress = ProjectPekerjaan::select('project_pekerjaan.id', 'project_pekerjaan.deskripsi_subkategori', 'project_pekerjaan.status', 'project_pekerjaan.id_subkategori', 'sub_kategori.name')
+                        ->join('sub_kategori', 'project_pekerjaan.id_subkategori', '=', 'sub_kategori.id')
+                        ->where('project_pekerjaan.id_project', $id_project)
+                        ->where('project_pekerjaan.id_kategori', $id_kategori)
+                        ->groupBy('project_pekerjaan.id', 'project_pekerjaan.deskripsi_subkategori', 'project_pekerjaan.status', 'sub_kategori.name')
+                        ->filter($request)
+                        ->get();
                 
             foreach ($progress as $item) {
                 $item->setAttribute('name', $item->subkategori->name . " " . $item->deskripsi_subkategori);

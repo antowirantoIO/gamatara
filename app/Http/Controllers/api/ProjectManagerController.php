80c5cb50191ff7908b726bb0bc01a175148895b3
asukayuki7 <asukayuki7@gmail.com>
2023-11-23 09:35:58 +0700

    fixing

--- app/Http/Controllers/api/ProjectManagerController.php
+++ app/Http/Controllers/api/ProjectManagerController.php
@@ -144,7 +144,7 @@ class ProjectManagerController extends Controller
 
             $progress = ProjectPekerjaan::where('id_project', $request->id_project)
                 ->where('id_kategori', $request->id_kategori)
-                ->select('id_subkategori', DB::raw('MAX(status) as max_status'))
+                ->select('id_subkategori', DB::raw('MAX(status) as max_status'), DB::raw('MAX(deskripsi_pekerjaan) as deskripsi_pekerjaan'))
                 ->groupBy('id_subkategori')
                 ->filter($request)
                 ->get();
@@ -161,6 +161,7 @@ class ProjectManagerController extends Controller
                 $matchingProgress = $progress->firstWhere('id_subkategori', $item->id);
 
                 if ($matchingProgress) {
+                    $item->setAttribute('name', $item->name . " " . $matchingProgress->deskripsi_pekerjaan);
                     $maxStatus = $matchingProgress->max_status;
 
                     if ($maxStatus == 1) {

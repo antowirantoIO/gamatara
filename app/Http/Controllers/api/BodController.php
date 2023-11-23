a71bd3703883a061d6e51995b010613a70b085e3
asukayuki7 <asukayuki7@gmail.com>
2023-11-23 18:01:53 +0700

    fixing api

--- app/Http/Controllers/api/BodController.php
+++ app/Http/Controllers/api/BodController.php
@@ -207,19 +207,25 @@ class BodController extends Controller
     public function index(Request $request)
     {
         try{
-            $data = OnRequest::with(['progress:id,id_project,id_vendor','progress.vendors:id,name','customer:id,name'])
-                    ->select('id','nama_project','created_at','id_customer','status')
-                    ->where('status',1)
-                    ->get();
+            $data = OnRequest::with(['complaint','customer:id,name'])
+                ->select('A.Nama_Project', 'A.created_at', 'A.id','A.id_customer',
+                    DB::raw('(SELECT COUNT(id_Pekerjaan) FROM project_pekerjaan WHERE status = 3 AND id_project = A.id) AS done'), 
+                    DB::raw('(SELECT COUNT(id_Pekerjaan) FROM project_pekerjaan WHERE id_project = A.id) AS total'), 'A.status')
+                ->from('Project as A')
+                ->leftJoin('project_pekerjaan as b', 'A.id', '=', 'b.id_project')
+                ->where('A.status', 1)
+                ->groupBy('A.id')
+                ->orderByDesc('A.created_at')
+                ->get();
 
             foreach ($data as $item) {
-                $item['nama_customer'] = $item->customer->name ?? '';
+                $item['nama_customer'] = $item->customer->name ?? '-';
                 $item['tanggal'] = $item->created_at ? date('d M Y', strtotime($item->created_at)) : '-';
-                $item['progress_pekerjaan'] = getProgresProject($item->id) . ' / ' . getCompleteProject($item->id);
+                $item['progress_pekerjaan'] = $item->done . ' / ' .$item->total;
 
                 if ($item->complaint->isEmpty()) {
                     $status = 1;
-                } elseif ($item->complaint->where('id_pm_approval','!==', null)->isNotEmpty() && $item->complaint->where('id_bod_approval', null)->isNotEmpty()) {
+                } elseif ($item->complaint->where('id_pm_approval', null)->isNotEmpty() && $item->complaint->where('id_pm_approval', null)->isNotEmpty()) {
                     $status = 2;
                 } else {
                     $status = 3;

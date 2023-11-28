7211e2b50b8f5f4ce101350f2c2c075192d6c0b8
Haris Maulana <99maulanaaris@gmail.com>
2023-11-28 14:45:00 +0700

    Bug Fixed Progress Pekerjaan Vendor

--- resources/views/on_progres/pekerjaan_vendor/index.blade.php
+++ resources/views/on_progres/pekerjaan_vendor/index.blade.php
@@ -8,22 +8,24 @@
                 <div class="col-12">
                     <div class="d-flex align-items-center flex-lg-row flex-column">
                         <div class="flex-grow-1 d-flex align-items-center">
-                            <a href="{{route('on_progress.edit',$project)}}">
+                            <a href="{{route('on_progres.tagihan.all',$id)}}">
                                 <i><img src="{{asset('assets/images/arrow-left.svg')}}" style="width: 20px;"></i>
                             </a>
-                            <h4 class="mb-0 ml-2"> &nbsp; Vendor Jobs</h4>
+                            <h4 class="mb-0 ml-2"> &nbsp; Vendor Bills</h4>
                         </div>
                     </div>
                 </div>
             </div>
 
+
+
             <div class="row">
                 <div class="col-lg-12">
                     <div class="d-flex justify-content-between">
                         <ul class="nav nav-tabs gap-3" id="myTab" role="tablist">
-                            @foreach ($kategori as $key => $item)
+                            @foreach ($workers as $key => $worker)
                                 <li class="nav-item" role="presentation">
-                                    <button class="nav-link {{ $loop->first ? 'active' : '' }} rounded-pill" id="{{ $item->id }}-tab" data-bs-toggle="tab" data-bs-target="#kategori-{{ $item->id }}" type="button" role="tab" aria-controls="{{ $item->id }}" aria-selected="true">{{ $item->name }}</button>
+                                    <button class="nav-link btn-filter-category {{ $loop->first ? 'active' : '' }} rounded-pill" data-category_id="{{ $worker->id }}"  data-name="{{ $worker->name }}" data-bs-target="#{{ $key }}" type="button">{{ $worker->name }}</button>
                                 </li>
                             @endforeach
                         </ul>
@@ -31,20 +33,20 @@
                     <div class="card mt-3">
                         <div class="card-body">
                             <div class="live-preview">
-                                @foreach ($kategori as $keys => $items)
-                                    <div class="tab-content" id="myTabContent">
-                                        <div class="tab-pane fade {{ $loop->first ? 'show active' : '' }}" id="kategori-{{ $items->id }}" role="tabpanel" aria-labelledby="{{ $items->id }}-tab">
+                                <div class="col-md-12">
+                                    <div>
+                                        <div class="tab-pane" role="tabpanel">
                                             <div class="d-flex justify-content-between align-items-center mb-3">
-                                                <span class="fs-5"><strong>Pekerjaan {{ getNameKategori($items->id) }}</strong></span>
+                                                <span class="fs-5"><strong>Pekerjaan</strong></span>
                                                 <div>
-                                                    <button class="btn btn-secondary" id="btn-fillter-{{ $keys }}">
+                                                    <button class="btn btn-secondary" id="btn-fillter">
                                                         <span>
                                                             <i><img src="{{asset('assets/images/filter.svg')}}" style="width: 15px;"></i>
                                                         </span> &nbsp; Filter
                                                     </button>
                                                 </div>
                                             </div>
-                                            <table class="table w-100" id="tableData{{ $items->id }}">
+                                            <table class="table w-100" id="tableData">
                                                 <thead class="table-light">
                                                     <tr>
                                                         <th style="color:#929EAE;width:600px;">Job</th>
@@ -54,17 +56,11 @@
                                                     </tr>
                                                 </thead>
                                                 <tbody>
-                                                    @foreach ($subWorker as $key => $worker)
-                                                        @foreach ($worker as $value)
-                                                            <input type="text" class="d-none id_kategori {{ $loop->first ? 'active' : '' }}" id="id_kategori-{{ $key }}" value="{{ $value->id_kategori }}">
-                                                            <input type="text" class="d-none id_project {{ $loop->first ? 'active' : '' }}" id="id_project-{{ $key }}" value="{{ $value->id_project }}">
-                                                        @endforeach
-                                                    @endforeach
                                                 </tbody>
                                             </table>
                                         </div>
                                     </div>
-                                @endforeach
+                                </div>
                             </div>
                         </div>
                     </div>
@@ -74,8 +70,6 @@
         </div>
     </div>
 </div>
-
-{{-- modal --}}
 
 <div id="modalFillter" class="modal fade zoomIn" tabindex="-1" aria-labelledby="zoomInModalLabel" aria-hidden="true" style="display: none;">
     <div class="modal-dialog modal-dialog-top-right">
@@ -99,13 +93,8 @@
                     </div>
                     <div class="col-xxl-6 col-md-6">
                         <div>
-                            <label for="nama_vendor" class="form-label">Vendor Name</label>
-                            <select name="nama_vendor" id="nama_vendor" class="form-select">
-                                <option value="">Choose Vendor</option>
-                                @foreach($vendor as $v)
-                                <option value="{{$v->id}}">{{$v->name}}</option>
-                                @endforeach
-                            </select>
+                            <label for="id_lokasi" class="form-label">Location Name</label>
+                            <input name="id_lokasi" id="id_lokasi" class="form-control"/>
                         </div>
                     </div>
                 </div>
@@ -121,126 +110,113 @@
 @endsection
 
 @section('scripts')
-
     <script>
         $(document).ready(function(){
+            let filterData = {};
             let modalInput = $('#modalFillter');
+            let btnFilterCategory = $('.btn-filter-category');
+
+            var table = $('#tableData').DataTable({
+                fixedHeader:true,
+                scrollX: true,
+                processing: true,
+                serverSide: true,
+                searching: false,
+                bLengthChange: false,
+                language: {
+                    processing:
+                        '<div class="spinner-border text-info" role="status">' +
+                        '<span class="sr-only">Loading...</span>' +
+                        "</div>",
+                    paginate: {
+                        Search: '<i class="icon-search"></i>',
+                        first: "<i class='fas fa-angle-double-left'></i>",
+                        next: "Next <span class='mdi mdi-chevron-right'></span>",
+                        last: "<i class='fas fa-angle-double-right'></i>",
+                    },
+                    "info": "Displaying _START_ - _END_ of _TOTAL_ result",
+                },
+                drawCallback: function() {
+                    var previousButton = $('.paginate_button.previous');
+                    previousButton.css('display', 'none');
+                },
+                ajax : {
+                    url : '{{ route('ajax.progres-pekerjaan-vendor') }}',
+                    method : 'GET',
+                    data : function(d){
+                        d._token = '{{ csrf_token() }}';
+                        d.id_project = '{{ $project }}';
+                        d.id_kategori = filterData.category_id;
+                        d.sub_kategori = $('#sub_kategori').val();
+                        d.id_lokasi = $('#id_lokasi').val();
+                        d.id_vendor = '{{ $id }}'
+                    }
+                },
+                columns : [
+                    { data : 'pekerjaan'},
+                    { data : 'progres'},
+                    { data : 'vendors.name'},
+                    {
+                        data : function(data){
+                            let id_vendor = data.id_vendor;
+                            let id_project = data.id_project;
+                            let id_subkategori = data.id_subkategori;
+                            let id_kategori = data.id_kategori;
+                            let url = '{{ route('on_progres.vendor-worker',[':id',':project',':subkategori',':idKategori']) }}';
+                            let urlReplace = url.replace(':id',id_vendor).replace(':project',id_project).replace(':subkategori',id_subkategori).replace(':idKategori',id_kategori);
+                            return `<a href="${urlReplace}" class="btn btn-warning btn-sm">
+                                <span>
+                                    <i><img src="{{asset('assets/images/eye.svg')}}" style="width: 15px;"></i>
+                                </span>
+                            </a>`
+                        }
+                    }
+                ]
+            })
+            handleFiltertab($('.btn-filter-category.active'));
+
+            btnFilterCategory.on('click', function(){
+                handleFiltertab($(this))
+            })
+
+            function handleFiltertab(element){
+                const categoryId = element.data('category_id'),
+                      name = element.data('name');
+                btnFilterCategory.removeClass('active')
+                element.addClass('active');
+                filterData = {
+                    category_id : categoryId,
+                    name_project: name
+                }
+                table.draw();
+            }
 
             $('.form-select').select2({
                 theme : "bootstrap-5",
-                search: true
+                search: true,
+                dropdownParent: $("#modalFillter")
+            });
+            $('#btn-fillter').click(function(){
+                modalInput.modal('show');
+            })
+            $('#btn-search').on('click',function(e){
+                e.preventDefault();
+                table.draw();
             });
 
-            @foreach ($kategori as $key => $worker)
 
-                var id_kategori = $('#id_kategori-{{ $key }}').val();
-                var id_project = $('#id_project-{{ $key }}').val();
-
-                var table{{ $key }} = $('#tableData{{ $key }}').DataTable({
-                    fixedHeader:true,
-                    scrollX: false,
-                    bSort : false,
-                    processing: true,
-                    serverSide: true,
-                    searching: false,
-                    bLengthChange: false,
-                    language: {
-                        processing:
-                            '<div class="spinner-border text-info" role="status">' +
-                            '<span class="sr-only">Loading...</span>' +
-                            "</div>",
-                        paginate: {
-                            Search: '<i class="icon-search"></i>',
-                            first: "<i class='fas fa-angle-double-left'></i>",
-                            next: "Next <span class='mdi mdi-chevron-right'></span>",
-                            last: "<i class='fas fa-angle-double-right'></i>",
-                        },
-                        "info": "Displaying _START_ - _END_ of _TOTAL_ result",
-                    },
-                    drawCallback: function() {
-                        var previousButton = $('.paginate_button.previous');
-                        previousButton.css('display', 'none');
-                    },
-                    ajax : {
-                        url : '{{ route('ajax.progres-pekerjaan-vendor') }}',
-                        method : 'GET',
-                        data : function(d){
-                            d._token = '{{ csrf_token() }}';
-                            d.id_kategori = id_kategori;
-                            d.id_project = id_project;
-                            d.id_vendor = '{{ $id }}';
-                            d.sub_kategori = $('#sub_kategori').val();
-                            d.nama_vendor = $('#nama_vendor').val();
-                        }
-                    },
-                    columns : [
-                        { data : 'pekerjaan'},
-                        { data : 'progres'},
-                        { data : 'vendors.name'},
-                        {
-                            data : function(data){
-                                let id_vendor = data.id_vendor;
-                                let id_project = data.id_project;
-                                let id_subkategori = data.id_subkategori;
-                                let id_kategori = data.id_kategori;
-                                let url = '{{ route('on_progres.vendor-worker',[':id',':project',':subkategori',':idKategori']) }}';
-                                let urlReplace = url.replace(':id',id_vendor).replace(':project',id_project).replace(':subkategori',id_subkategori).replace(':idKategori',id_kategori);
-                                return `<a href="${urlReplace}" class="btn btn-warning btn-sm">
-                                    <span>
-                                        <i><img src="{{asset('assets/images/eye.svg')}}" style="width: 15px;"></i>
-                                    </span>
-                                </a>`
-                            }
-                        }
-                    ]
-                });
-
-                $('#btn-fillter-{{ $key }}').click(function(){
-                    modalInput.modal('show');
-                })
-                $('#btn-search').on('click',function(e){
-                    e.preventDefault();
-                    modalInput.modal('hide');
-                    var active = $('#myTabContent .tab-pane.active');
-                    id_kategori = active.find('.id_kategori.active').val();
-                    id_project = active.find('.id_project.active').val();
-
-                    var activeTabId = $('#myTab .nav-link.active').attr('id');
-                    if (activeTabId === '{{ $key }}-tab') {
-                        table{{ $key }}.draw();
-                    }
-                });
-
-
-                $('#btn-reset').click(function(e){
-                    e.preventDefault();
-                    $('.form-control').val('');
-                    $('.form-select').val(null).trigger('change');
-                    var activeTabId = $('#myTab .nav-link.active').attr('id');
-                    if (activeTabId === '{{ $key }}-tab') {
-                        table{{ $key }}.draw();
-                    }
-                })
-            @endforeach
-            function hideOverlay() {
-                $('.loading-overlay').fadeOut('slow', function() {
-                    $(this).remove();
-                });
+            $('#btn-reset').click(function(e){
+                e.preventDefault();
+                $('.form-control').val('');
+                $('.form-select').val(null).trigger('change');
+                table.draw();
+            })
+            const rupiah = (number)=>{
+                var	reverse = number.toString().split('').reverse().join(''),
+                ribuan 	= reverse.match(/\d{1,3}/g);
+                ribuan	= ribuan.join('.').split('').reverse().join('');
+                return ribuan;
             }
-
-            $('.export-button').on('click', function(event) {
-                event.preventDefault();
-                var id_project      = '{{ $project }}';
-                var url = '{{ route("on_progress.export-pekerjaan") }}?' + $.param({
-                    id_project: id_project,
-                });
-
-                $('.loading-overlay').show();
-
-                window.location.href = url;
-
-                setTimeout(hideOverlay, 2000);
-            });
         })
     </script>
 @endsection
